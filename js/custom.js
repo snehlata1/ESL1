92baed0b2b6191258e57d71bbaae71ac755f5fd4
Sneh Lata <snehlata.ewb@gmail.com>
2020-01-18 16:32:54 +0530

    section add

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,39 @@ $('.cl-full').slick({
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
+    nextArrow: '<button class="next-arrow">  <img src="./images/virustotal-icon.png" alt="slider-arrow-right"> </button>',
+    prevArrow: '<button class="back-arrow"><img src="./images/left-chevron.png" alt="slider-arrow-left"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
+
+  $('.outer').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    nextArrow: '<button class="next-arrow">  <img src="./images/next.png" alt="slider-arrow-right"> </button>',
+    prevArrow: '<button class="back-arrow"><img src="./images/next.png" alt="slider-arrow-left"></button>',
     responsive: [
       {
         breakpoint: 768,
