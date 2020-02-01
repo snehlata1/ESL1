2829db73c8e04197af2c81d8271229982cb5c0d1
Sneh Lata <snehlata.ewb@gmail.com>
2020-02-01 14:27:57 +0530

    responsive completed

--- js/custom.js
+++ js/custom.js
@@ -1,61 +1,57 @@
-$('.cl-full').slick({
-    centerMode: true,
-    centerPadding: '60px',
+
+  $('.cl-full').slick({
     slidesToShow: 3,
-    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
     nextArrow: '<button class="next-arrow">  <img src="./images/virustotal-icon.png" alt="slider-arrow-right"> </button>',
     prevArrow: '<button class="back-arrow"><img src="./images/left-chevron.png" alt="slider-arrow-left"></button>',
     responsive: [
+
       {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+       breakpoint: 992,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+       }
       },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-
-
-
+     {
+       breakpoint: 525,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+       }
+     }
+   
+     ]
+     
+     });
 
   $('.outer').slick({
-    centerMode: true,
-    centerPadding: '60px',
     slidesToShow: 1,
-    speed: 300,
-    dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
     nextArrow: '<button class="next-arrow">  <img src="./images/next.png" alt="slider-arrow-right"> </button>',
     prevArrow: '<button class="back-arrow"><img src="./images/next.png" alt="slider-arrow-left"></button>',
     responsive: [
+
       {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
+       breakpoint: 992,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+       }
       },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+     {
+       breakpoint: 525,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1
+       }
+     }
+   
+     ]
+     
+     });
+ 
